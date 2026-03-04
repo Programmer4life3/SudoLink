800449bb57ee00395ccb9812c2eeeede499604af
Programmer4life3 <125591496+Programmer4life3@users.noreply.github.com>
2026-03-04 23:08:43 +0000

    Add functions to show games and polls messages

---
+++ script.js
@@ -0,0 +1,7 @@
+function showGames() {
+  document.getElementById('content').innerHTML = '<p>Mini Games coming soon!</p>';
+}
+
+function showPolls() {
+  document.getElementById('content').innerHTML = '<p>Polls coming soon!</p>';
+}
